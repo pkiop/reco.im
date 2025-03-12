593f631c7564908237b149e0d8a914f3fc939d7d
pkiop <pkiopb@gmail.com>
2025-03-12 21:59:39 +0900

    test

---
+++ controllers/Blog.cc
@@ -0,0 +1,12 @@
+#include "Blog.h"
+
+void Blog::asyncHandleHttpRequest(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback)
+{
+    // write your application logic here
+    auto resp=HttpResponse::newHttpResponse();
+    //NOTE: The enum constant below is named "k200OK" (as in 200 OK), not "k2000K".
+    resp->setStatusCode(k200OK);
+    resp->setContentTypeCode(CT_TEXT_HTML);
+    resp->setBody("Hello World!");
+    callback(resp);
+}
